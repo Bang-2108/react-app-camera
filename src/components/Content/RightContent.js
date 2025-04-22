21bbb74ca0cb7f3d2e192be77de2cc3b4bc7de09
Bang-2108 <zoanthibang@gmail.com>
2025-04-22 09:26:56 +0700

    props api

--- src/components/Content/RightContent.js
+++ src/components/Content/RightContent.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import ShowCard from './ShowCard/ShowCard';
-import { getData } from './data.js';
+
 class RightContent extends Component {
+    state = {
+        products: []
+    };
+    componentDidMount() {
+        axios.get('https://656ca88ee1e03bfd572e9c16.mockapi.io/products')
+            .then(response => {
+                this.setState({ products: response.data });
+            })
+            .catch(error => {
+                console.error('Error fetching products:', error);
+            });
+    }
+
     render() {
-        console.log(getData());
-        const products = getData();
+        const { products } = this.state;
+
         return (
             <div>
                 <div id="right-content">
-                    <h2>Product : </h2>
+                    <h2>Product :</h2>
                     <div id="products">
                         {
                             products.map(product =>
-                                <ShowCard loai={product.loai} name={product.name} image={product.image} />)}
+                                <ShowCard
+                                    key={product.id}  // Luôn cần key khi map
+                                    loai={product.loai}
+                                    name={product.name}
+                                    image={product.image}
+                                />
+                            )
+                        }
                         <div style={{ clear: 'both' }} />
                     </div>
                     <div style={{ clear: 'both' }} />
@@ -22,4 +43,5 @@ class RightContent extends Component {
         );
     }
 }
-export default RightContent;						
+
+export default RightContent;
