c59e9a8892a2222dec168b3831bd04e0e1086be9
Bang-2108 <zoanthibang@gmail.com>
2025-04-24 09:33:45 +0700

    get data

--- src/components/Content/RightContent.js
+++ src/components/Content/RightContent.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ShowCard from './ShowCard/ShowCard';
 
-class RightContent extends Component {
-    state = {
-        products: []
-    };
-    componentDidMount() {
+
+const RightContent = () => {
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
         axios.get('https://656ca88ee1e03bfd572e9c16.mockapi.io/products')
             .then(response => {
-                this.setState({ products: response.data });
+                setProducts(response.data);
             })
             .catch(error => {
                 console.error('Error fetching products:', error);
             });
-    }
+    }, []);
 
-    render() {
-        const { products } = this.state;
 
-        return (
-            <div>
-                <div id="right-content">
-                    <h2>Product :</h2>
-                    <div id="products">
-                        {
-                            products.map(product =>
-                                <ShowCard
-                                    key={product.id}  // Luôn cần key khi map
-                                    loai={product.loai}
-                                    name={product.name}
-                                    image={product.image}
-                                />
-                            )
-                        }
-                        <div style={{ clear: 'both' }} />
-                    </div>
+    return (
+        <div>
+            <div id="right-content">
+                <h2>Product :</h2>
+                <div id="products">
+                    {
+                        products.map(product =>
+                            <ShowCard
+                                key={product.id}  
+                                loai={product.loai}
+                                name={product.name}
+                                image={product.image}
+                            />
+                        )
+                    }
                     <div style={{ clear: 'both' }} />
                 </div>
                 <div style={{ clear: 'both' }} />
             </div>
-        );
-    }
+            <div style={{ clear: 'both' }} />
+        </div>
+    );
 }
+
 
 export default RightContent;
